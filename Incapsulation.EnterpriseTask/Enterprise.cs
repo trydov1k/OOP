c2c08a521206bd816c7d65b8977e47144971f1de
Дмитрий <mdimon00@mail.ru>
2026-09-23 14:10:31 +0500

    Выполнил задание InterpriseTask

--- Incapsulation.EnterpriseTask/Enterprise.cs
+++ Incapsulation.EnterpriseTask/Enterprise.cs
@@ -1,55 +1,28 @@
 ﻿namespace Incapsulation.EnterpriseTask;
 
-public class Enterprise
+public class Enterprise(Guid guid)
 {
-	Guid guid;
-
-	public Guid getGuid() { return guid; }
-
-	public Enterprise(Guid guid)
+	public readonly Guid Guid = guid;
+	public string Name { get; set; } = string.Empty;
+	private string inn = string.Empty;
+	public string Inn
 	{
-		this.guid = guid;
+		get => inn;
+		set
+		{
+			if (value.Length != 10 || !value.All(z => char.IsDigit(z)))
+				throw new ArgumentException();
+			inn = value;
+		}
 	}
+	public DateTime EstablishDate { get; set; }
+	public TimeSpan ActiveTimeSpan => DateTime.Now - EstablishDate;
 
-	string name;
-
-	public string getName() { return name; }
-
-	public void setName(string name) { this.name = name; }
-
-	string inn;
-
-	public string getINN() { return inn; }
-
-	public void setINN(string inn)
-	{
-		if (inn.Length != 10 || !inn.All(z => char.IsDigit(z)))
-			throw new ArgumentException();
-		this.inn = inn;
-	}
-
-	DateTime establishDate;
-
-	public DateTime getEstablishDate()
-	{
-		return establishDate;
-	}
-
-	public void setEstablishDate(DateTime establishDate)
-	{
-		this.establishDate = establishDate;
-	}
-
-	public TimeSpan getActiveTimeSpan()
-	{
-		return DateTime.Now - establishDate;
-	}
-
-	public double getTotalTransactionsAmount()
+	public double GetTotalTransactionsAmount()
 	{
 		DataBase.OpenConnection();
 		var amount = 0.0;
-		foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == guid))
+		foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == this.Guid))
 			amount += t.Amount;
 		return amount;
 	}
